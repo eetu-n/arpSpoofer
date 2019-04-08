cf31d05490d8b66e2087964421b022f7c25cd44a
rargh <eetu.t.narhi@gmail.com>
2019-04-08 15:33:13 +0200

    Added destination class

--- DataStructures.py
+++ DataStructures.py
@@ -1,5 +1,8 @@
+import socket
+
+
 class Interface:
-    def __init__(self, name, netmask, addr, hwaddr):
+    def __init__(self, name: str, netmask: str, addr: str, hwaddr: str):
         self.name = name
         self.netmask = netmask
         self.addr = addr
@@ -23,7 +26,7 @@ class Interface:
 
 
 class Host:
-    def __init__(self, addr, hwaddr):
+    def __init__(self, addr: str, hwaddr: str):
         self.addr = addr
         self.hwaddr = hwaddr
 
@@ -32,3 +35,25 @@ class Host:
 
     def get_hwaddr(self):
         return self.hwaddr
+
+
+class Destination:
+    def __init__(self, url: str = ""):
+        self.ip_list = self.find_ip_list(url)
+        self.url = url
+
+    def get_ip_list(self):
+        return self.ip_list
+
+    def get_url(self):
+        return self.url
+
+    @staticmethod
+    def find_ip_list(url):
+        ip_list = []
+        for result in socket.getaddrinfo(url, 0, 0, 0, 0):
+            result_ip = result[-1][0]
+            if result_ip not in ip_list:
+                ip_list.append(result_ip)
+
+        return ip_list
